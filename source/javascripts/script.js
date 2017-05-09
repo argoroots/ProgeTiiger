1c9fd3b352c01229c00482d2e72cb475b9134f04
Argo Roots <argo@roots.ee>
2017-05-09 13:25:58 +0300

    Routing restored

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -59,23 +59,23 @@ angular.module('pdApp', ['ngRoute', 'ngResource'])
     .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
         // $locationProvider.html5Mode(true)
         $routeProvider
-            // .when('/voting', {
-            //     templateUrl: 'start',
-            //     controller: 'startCtrl'
-            // })
-            // .when('/voting/:voter_id/:voter_key', {
-            //     templateUrl: 'voting',
-            //     controller: 'votingCtrl'
-            // })
+            .when('/voting', {
+                templateUrl: 'start',
+                controller: 'startCtrl'
+            })
+            .when('/voting/:voter_id/:voter_key', {
+                templateUrl: 'voting',
+                controller: 'votingCtrl'
+            })
             .when('/end', {
                 templateUrl: 'end',
             })
-            // .otherwise({
-            //     redirectTo: '/voting'
-            // })
             .otherwise({
-                redirectTo: '/end'
+                redirectTo: '/voting'
             })
+            // .otherwise({
+            //     redirectTo: '/end'
+            // })
     }])
 
     .filter('orderObjectBy', function(){
